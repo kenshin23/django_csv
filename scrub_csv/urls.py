9f38ae31b85072578ce00dedb598bf21280b30e6
Carlos Paparoni <carlos.paparoni@gmail.com>
2015-04-15 10:49:35 -0430

    Basic views' structure

---
+++ scrub_csv/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # File processing views:
+    # ex: /files/
+    url(r'^$', views.index, name='index'),
+
+    # ex: /files/new/ -- This lets the user upload a file
+    url(r'^new/$', views.new, name='new'),
+
+    # ex: /files/5/ -- This lets the user see relevant file info
+    url(r'^(?P<document_id>[0-9]+)/$',
+        views.detail, name='detail'),
+
+    # ex: /files/5/select/ -- This lets the user map file headers
+    # to database fields
+    url(r'^(?P<document_id>[0-9]+)/select/$',
+        views.select_fields, name='select_fields'),
+
+    # ex: /files/5/process/ -- This actually processes the file
+    url(r'^(?P<document_id>[0-9]+)/process/$',
+        views.process, name='process'),
+
+    # ex: /files/5/records/ -- This shows the processing result
+    url(r'^(?P<document_id>[0-9]+)/records/$',
+        views.records, name='records'),
+]
--- scrub_csv/views.py
+++ scrub_csv/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the scrubber index.")
+
+
+def new(request):
+    return HttpResponse("You're looking at the file upload page.")
+
+
+def detail(request, document_id):
+    return HttpResponse("You're looking at file %s." % document_id)
+
+
+def records(request, document_id):
+    response = "You're looking at the records of document %s."
+    return HttpResponse(response % document_id)
+
+
+def select_fields(request, document_id):
+    response = "This is step 1 for the document %s processing page."
+    return HttpResponse(response % document_id)
+
+
+def process(request, document_id):
+    response = "This is step 2 for the document %s processing page."
+    return HttpResponse(response % document_id)
